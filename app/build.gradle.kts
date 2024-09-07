dbeab7e74a646a2aa3207c20b253a28da9d1ad3a
Mahamad Charawi <mahamad@laudspeaker.com>
2024-09-07 11:38:22 -0700

    Upgrade gradle to fix build.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.laudspeaker.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.laudspeaker.myapplication"
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,21 @@ plugins {
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0-Beta5" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-Beta5")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
