70bcc255ed6f0dd4fda08415d3f24199e9ae4ad4
Abheek Basu <abheek94@gmail.com>
2024-09-11 22:01:10 -0400

    sending and recieving some notifications, foreground not working

--- app/src/main/java/com/laudspeaker/myapplication/AppModule.kt
+++ app/src/main/java/com/laudspeaker/myapplication/AppModule.kt
@@ -8,8 +8,8 @@ val appModule = module {
     single {
         val config = LaudspeakerAndroidConfig(
             this.androidContext(),
-            "jRLW4EHcKWo5mHQfRBA3VQ34pcOenhZsBbUtBHhL",
-            "https://8dd0db94cec8.ngrok.app",
+            "WjzfCfRwSXDtG3H6u72b4PGate0B1a4MDhu51j0x",
+            "https://b25df60f2464.ngrok.app", //"https://app.laudspeaker.com/api",
             MainActivity::class.java // Might need to adjust based on actual usage
         )
         LaudspeakerAndroid.with(get(), config)
